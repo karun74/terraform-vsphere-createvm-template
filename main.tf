cc5c1b8f1d1ffe4e6138bd3bbb87048affe6e5ce
Shanmugam <Karunanithi.Shanmugam@emc.com>
2021-02-08 12:55:54 -0600

    added vm template name as variable

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "vsphere_network" "network" {
 
 
 data "vsphere_virtual_machine" "template" {
-  name          = "photon-template"
+  name          = var.vm_template_name 
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
